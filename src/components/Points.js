d6d5012b9438ce0715540bbb07b0fae4ae998106
anujeshyadav <114899681+anujeshyadav@users.noreply.github.com>
2022-11-21 15:47:33 +0530

    new

--- src/components/Points.js
+++ src/components/Points.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Row, Col } from "reactstrap";
 
 function Points() {
+  const [getyourpoint, setGetyourpoint] = useState({});
+  useEffect(() => {
+    earnpoints();
+  }, []);
+
+  const earnpoints = () => {
+    const userId = localStorage.getItem("userId");
+
+    axios
+      .get(`http://3.7.173.138:9000/user/my_content_meteros/${userId}`)
+      .then((res) => {
+        setGetyourpoint(res.data);
+      })
+      .catch((err) => {
+        console.log(err.data);
+      });
+  };
   return (
-    <div>Total points Earned :</div>
-  )
+    <>
+      <h3 className="d-flex justify-content-center mt-3">
+        Your Earning's here
+      </h3>
+      <Row className="mt-5">
+        <Col lg="2">
+          <h5 className="d-flex justify-content-left">Earned Points : </h5>
+        </Col>
+        <Col>
+          <h5>{getyourpoint?.meteors} Points</h5>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default Points
+export default Points;
